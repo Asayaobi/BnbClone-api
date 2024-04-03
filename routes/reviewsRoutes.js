5a6ae9a294efa587ab565e4b011f27d46adf4ece
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-04-03 16:58:41 +0700

    added get request

--- routes/reviewsRoutes.js
+++ routes/reviewsRoutes.js
@@ -60,46 +60,83 @@ router.post('/reviews', async (req, res) => {
 })
 
 // Define a GET route for fetching a single review
-router.get('/reviews/:reviewId', async (req, res) => {
-  try {
-    let reviewId = Number(req.params.reviewId)
-    if (!reviewId) {
-      throw new Error('Please insert a number')
-    }
-    const { rows } = await db.query(
-      `SELECT * FROM reviews WHERE review_id = ${req.params.reviewId}`
-    )
-    if (rows.length === 0) {
-      throw new Error(`No review found with id ${req.params.reviewId}`)
-    }
-    console.log(rows)
-    res.json(rows)
-  } catch (err) {
-    console.error(err.message)
-    res.json(err.message)
-  }
-})
+// router.get('/reviews/:reviewId', async (req, res) => {
+//   try {
+//     let reviewId = Number(req.params.reviewId)
+//     if (!reviewId) {
+//       throw new Error('Please insert a number')
+//     }
+//     const { rows } = await db.query(
+//       `SELECT * FROM reviews WHERE review_id = ${req.params.reviewId}`
+//     )
+//     if (rows.length === 0) {
+//       throw new Error(`No review found with id ${req.params.reviewId}`)
+//     }
+//     console.log(rows)
+//     res.json(rows)
+//   } catch (err) {
+//     console.error(err.message)
+//     res.json(err.message)
+//   }
+// })
 
-// Define a GET route for fetching the list of reviews
 router.get('/reviews', async (req, res) => {
   try {
-    // query to sort reviews to show newest first
-    let queryReview = 'SELECT * FROM reviews ORDER BY review_date DESC'
-
-    //  query to return reviews that belong a specific house
-    if (req.query.house) {
-      queryReview = `SELECT * FROM reviews WHERE house_id = '${req.query.house}'
-      ORDER BY review_date DESC`
+    if (!req.query.house_id) {
+      throw new Error('house_id is required')
     }
 
-    const { rows } = await db.query(queryReview)
-    console.log(rows)
-    res.json(rows)
+    let sqlquery = `
+      SELECT reviews.*, users.first_name, users.last_name, users.profile_pic_url FROM reviews
+      LEFT JOIN users ON users.user_id = reviews.reviewer_id
+      WHERE house_id = ${req.query.house_id}
+      ORDER BY review_date DESC
+    `
+
+    let { rows } = await db.query(sqlquery)
+    const formatter = new Intl.DateTimeFormat('en-US', {
+      day: '2-digit',
+      month: 'short',
+      year: 'numeric'
+    })
+
+    let reviews = rows.map((r) => {
+      r.author = {
+        firstName: r.first_name,
+        lastName: r.last_name,
+        profile_pic_url: r.profile_pic_url
+      }
+      r.review_date = formatter.format(new Date(r.review_date))
+      delete r.first_name
+      delete r.last_name
+      delete r.profile_pic_url
+      return r
+    })
+    res.json(reviews)
   } catch (err) {
-    console.error(err.message)
-    res.json(err)
+    res.json({ error: err.message })
   }
 })
+// Define a GET route for fetching the list of reviews
+// router.get('/reviews', async (req, res) => {
+//   try {
+//     // query to sort reviews to show newest first
+//     let queryReview = 'SELECT * FROM reviews ORDER BY review_date DESC'
+
+//     //  query to return reviews that belong a specific house
+//     if (req.query.house) {
+//       queryReview = `SELECT * FROM reviews WHERE house_id = '${req.query.house}'
+//       ORDER BY review_date DESC`
+//     }
+
+//     const { rows } = await db.query(queryReview)
+//     console.log(rows)
+//     res.json(rows)
+//   } catch (err) {
+//     console.error(err.message)
+//     res.json(err)
+//   }
+// })
 
 //DELETE reviews
 router.delete('/reviews/:reviewId', async (req, res) => {
